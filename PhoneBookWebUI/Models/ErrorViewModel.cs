50f71b83bff6121ab108fb8f8f444e2a48cf515c
Ahmet <t3mh4@hotmail.com>
2022-08-12 06:33:06 +0300

    Initial commit

---
+++ PhoneBookWebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PhoneBookWebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
